e0c2b05aa21a9aded83a159e7dc602bdfa7e5100
Anton Kalchenko <anton11042002@gmail.com>
2023-03-16 21:02:14 +0200

    v3

--- birthday.py
+++ birthday.py
@@ -4,16 +4,22 @@ from datetime import datetime, timedelta
 def get_birthdays_per_week (users:list):
     all = {}
     today = datetime.today()
+
+    start_of_week = today - timedelta(days=today.weekday()) #Тиждень починається з понеділка
+    end_of_week = start_of_week + timedelta(days=7) #Функція виводить користувачів з днями народження на тиждень вперед від поточного дня
+
     for i in users: 
-        if today.strftime('%A') == 'Monday': 
-            today -= timedelta(days=2)
-        if i['birthday'] < today or i['birthday'] > today + timedelta(weeks=1): 
-            continue
-        else:
+
+        birthday = i['birthday']
+        birthday = birthday.replace(year=today.year)
+        
+        if start_of_week <= birthday <= end_of_week: 
             name = i['name']
             dayweek = i['birthday'].strftime('%A')
-            if dayweek == 'Sunday' or dayweek == 'Saturday':
+
+            if dayweek == 'Sunday' or dayweek == 'Saturday': #Користувачів, у яких день народження був на вихідних, потрібно привітати у понеділок.
                 dayweek = 'Monday'
+
             if dayweek not in all:
                 all[dayweek] = [name]
             else:
@@ -25,14 +31,14 @@ def get_birthdays_per_week (users:list):
 
 
 if __name__ == '__main__':
-    get_birthdays_per_week([
+    get_birthdays_per_week([ #Для відладки зручно створити тестовий список users та заповнити його самостійно.
           {'name': 'Bill',
-           'birthday': datetime(2023, 3, 16)},
+           'birthday': datetime(1976, 3, 16)},
           {'name': 'Atlas',
-           'birthday': datetime(2023, 3, 18)},
+           'birthday': datetime(1956, 3, 18)},
           {'name': 'Luka',
-           'birthday': datetime(2023, 3, 22)},
+           'birthday': datetime(2004, 3, 22)},
           {'name': 'Nola',
-           'birthday': datetime(2023, 3, 22)},
+           'birthday': datetime(2012, 3, 18)},
           {'name': 'Floss',
-           'birthday': datetime(2023, 3, 14)}])
+           'birthday': datetime(1945, 3, 14)}])
